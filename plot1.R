a72996dc4d57190882a68f442b849b9e27438697
IR236 <igor.rajic236@gmail.com>
2014-06-05 00:36:55 +0200

    plot1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("./household_power_consumption.txt", sep= ";", header = TRUE, stringsAsFactors = FALSE, na.string = "?")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+dfplot <- data[(data$Date == "2007-02-01") | (data$Date == "2007-02-02"),]
+png(filename = "plot1.png")
+hist(dfplot$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
